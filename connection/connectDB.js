f195d0e5cccb7ea9e2af88bb498bb6190a7cc2b3
TBZ-Projects2030 <tbzone.2030@gmail.com>
2025-03-20 13:41:14 +0500

    Update connectDB.js

--- connection/connectDB.js
+++ connection/connectDB.js
@@ -1,22 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // connect to the database
 const connectDB = async () => {
-    try {
-        // await mongoose.connect(process.env.DATABASE_URL);
-        const conn = await mongoose.connect(process.env.DATABASE_URL, {
-            useNewUrlParser: true, 
-          });
-           
-        console.log(
-            `DB connection successful! at ${conn.connection.host}`
-        );
-    } catch (err) {
-        console.log("some things went")
-        console.log(err);
-    }
+  try {
+    // await mongoose.connect(process.env.DATABASE_URL);
+    const conn = await mongoose.connect("mongodb+srv://1stStepsadmin:gT9SF8LXAHvEfwvS@cluster0.dns3kn4.mongodb.net/1stSteps?retryWrites=true&w=majority", {
+      useNewUrlParser: true,
+    });
+
+    console.log(`DB connection successful! at ${conn.connection.host}`);
+  } catch (err) {
+    console.log("some things went");
+    console.log(err);
+  }
 };
 
 module.exports = connectDB;
-
- 
