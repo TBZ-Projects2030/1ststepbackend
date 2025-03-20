6d326fbf35a071b01d6a77623ef95c702ed12c7c
TBZ-Projects2030 <tbzone.2030@gmail.com>
2025-03-20 13:41:54 +0500

    Update token.js

--- utils/token.js
+++ utils/token.js
@@ -1,23 +1,17 @@
-const jwt = require('jsonwebtoken');
-
-
+const jwt = require("jsonwebtoken");
 
 exports.generateToken = (userInfo) => {
-const payload = {
-    email: userInfo.email, 
+  const payload = {
+    email: userInfo.email,
     role: userInfo.role,
-    id: userInfo._id
+    id: userInfo._id,
+  };
+
+  const token = jwt.sign(payload, "efguywefweufdfedefhweufvfgeheiojwejdvtydgioxkdghwefdewfrefg", {
+    expiresIn: "5m",
+  });
+
+  return token;
 };
 
-const token = jwt.sign(payload, process.env.JWT_SECRET, {
-     expiresIn : '5m'
-})
-
-return token;
-};
-
-
-
-exports.verifyToken = (token) => {
-
-}
+exports.verifyToken = (token) => {};
